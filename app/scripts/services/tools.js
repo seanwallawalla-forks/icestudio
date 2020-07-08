4415ed7f4283787dc2a0067758668ce97f5eb220
Juan Manuel Rico <juanmard@gmail.com>
2020-07-08 02:33:27 +0200

    toolchain: show messages bar, fix installation completion

--- app/scripts/services/tools.js
+++ app/scripts/services/tools.js
@@ -35,7 +35,6 @@ angular
       var startAlert = null;
       var infoAlert = null;
       var resultAlert = null;
-      var toolchainAlert = null;
       var toolchain = {
         apio: '-',
         installed: false,
@@ -1078,7 +1077,7 @@ angular
 
       function installDefaultToolchain() {
         installationStatus();
-        toolchainAlert = alerts.alert({
+        alerts.alert({
           title: _tcStr('Installing toolchain'),
           body: `<div>
           <div class="progress">
@@ -1086,10 +1085,9 @@ angular
             aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">
             </div>
           </div>
-        </div>`,
-          onok: () => {
-            initProgress();
-          },
+          <div id="progress-message" class="progress-message"></div>
+      </div>`,
+          onok: utils.endWait,
         });
         toolchain.installed = false;
 
@@ -1107,7 +1105,7 @@ angular
 
       function installOnlineToolchain() {
         installationStatus();
-        toolchainAlert = alerts.alert({
+        alerts.alert({
           title: _tcStr('Installing toolchain'),
           body: `<div>
           <div class="progress">
@@ -1115,10 +1113,9 @@ angular
             aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">
             </div>
           </div>
+          <div id="progress-message" class="progress-message"></div>
         </div>`,
-          onok: () => {
-            initProgress();
-          },
+          onok: utils.endWait,
         });
         toolchain.installed = false;
 
@@ -1145,7 +1142,6 @@ angular
       function checkInternetConnection(callback) {
         updateProgress(_tcStr('Check Internet connection...'), 0);
         utils.isOnline(callback, function () {
-          closeToolchainAlert();
           restoreStatus();
           resultAlert = alertify.error(
             _tcStr('Internet connection required'),
@@ -1156,11 +1152,10 @@ angular
       }
 
       function ensurePythonIsAvailable(callback) {
-        updateProgress(_tcStr('Check Python...'), 0);
+        updateProgress(_tcStr('Check Python...'), 1);
         if (utils.getPythonExecutable()) {
           callback();
         } else {
-          closeToolchainAlert();
           restoreStatus();
           resultAlert = alertify.error(
             _tcStr('At least Python 3.5 is required'),
@@ -1262,7 +1257,6 @@ angular
       function installationCompleted(callback) {
         checkToolchain(function () {
           if (toolchain.installed) {
-            closeToolchainAlert();
             updateProgress(_tcStr('Installation completed'), 100);
             alertify.success(_tcStr('Toolchain installed'));
             setupDriversAlert();
@@ -1301,23 +1295,6 @@ angular
         bar.text(value + '%');
         bar.attr('aria-valuenow', value);
         bar.css('width', value + '%');
-      }
-
-      function initProgress() {
-        $('#progress-bar')
-          .addClass(
-            'notransition progress-bar-info progress-bar-striped active'
-          )
-          .removeClass('progress-bar-danger')
-          .text('0%')
-          .attr('aria-valuenow', 0)
-          .css('width', '0%')
-          .removeClass('notransition');
-      }
-
-      function closeToolchainAlert() {
-        toolchainAlert.callback();
-        toolchainAlert.close();
       }
 
       function installationStatus() {
